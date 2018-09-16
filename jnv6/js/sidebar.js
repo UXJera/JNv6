ad9fa24677d4dfce9039af17a5ab7cfdcc2663bc
Jeremiah Nyman <jeremiahnyman@gmail.com>
2018-09-15 18:37:31 -0700

    old refactor

--- jnv6/js/sidebar.js
+++ jnv6/js/sidebar.js
@@ -1,9 +1,29 @@
-$(document).ready(function() {
-  $('.sub-nav').on('click', function() {
-    $('.page-wrapper').toggleClass('isOpen');
+// $(document).ready(function() {
+//   $('.sub-nav').on('click', function() {
+//     $('.page-wrapper').toggleClass('isOpen');
+//   });
+//   $('.closing').on('click', function() {
+//     $('.page-wrapper').toggleClass('isOpen');
+//   });
+// });
 
-  });
-  $('.closing').on('click', function() {
-    $('.page-wrapper').toggleClass('isOpen');
-  });
-});
+const subnav = document.getElementByClass('sub-nav');
+const closing = document.getElementByClass('closing');
+const pageWrapper = document.getElementByClass('page-wrapper');
+subnav.addEventListener('click', toggleOpen());
+
+function toggleOpen() {
+  if (pageWrapper.classList) {
+    pageWrapper.classList.toggle(isOpen);
+  } else {
+    let classes = pageWrapper.className.split(' ');
+    let existingIndex = classes.indexOf(isOpen);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    pageWrapper.className = classes.join(' ');
+  }
+}
