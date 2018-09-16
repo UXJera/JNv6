759a0719af266f248d5f71bf0a3c352af3c9281f
Jera-Laptop <uxjera@gmail.com>
2018-09-15 18:35:41 -0700

    Changes for 6.1 starting: redoing several modifications to old website w/ new content

--- jnv6/js/sidebar.js
+++ jnv6/js/sidebar.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.sub-nav').on('click', function() {
     $('.page-wrapper').toggleClass('isOpen');
-
   });
   $('.closing').on('click', function() {
     $('.page-wrapper').toggleClass('isOpen');
